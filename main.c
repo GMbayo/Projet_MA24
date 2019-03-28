3e14864a0a10fd5f15e618c89284297ccbb9e1b9
MBAYO Guilain <Guilain.MBAYO@cpnv.ch>
2019-03-28 11:30:25 +0100

    Begin project

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 0, b = 0, c = 0, *p1, *p2;
+    fflush(stdin);
+    printf("Bonjour, nous allons utiliser les pointeurs, pour cela, vous devrez entrer 3 nombres.\n \n");
+    printf("Entrez le premier nombre: \n");
+    scanf("%i",&a);
+    fflush(stdin);
+    printf("\nEntrez le second nombre:\n");
+    scanf("%i",&b);
+    fflush(stdin);
+    printf("\nEntrez le dernier nombre:\n");
+    scanf("%i",&c);
+    system("cls");
+    printf("Voici maintenant les diff‚rentes op‚rations effectu‚es ainsi que les valeurs des diff‚rentes variables aprŠs chaque op‚ration:\n\n");
+    return 0;
+}
